4aa848899052ad681b41149293a684e2419bc5b5
water-su <water.su@me.com>
2020-07-01 13:27:47 +0800

    [removed]: to remove Main.storyboard from project [added]: create a new app entry point by xib [added]: to show a tableview on main ViewController with some dummy data

--- GitHubUsers/GitHubUsers/SceneDelegate.swift
+++ GitHubUsers/GitHubUsers/SceneDelegate.swift
@@ -17,7 +17,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = ViewController(nibName: "ViewController", bundle: nil)
+        let nav = UINavigationController(rootViewController: vc)
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+        window?.windowScene = windowScene
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- GitHubUsers/GitHubUsers/ViewController.swift
+++ GitHubUsers/GitHubUsers/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let cellId = "simpleCell"
+    
+    @IBOutlet weak var tableView: UITableView!{
+        didSet{
+            tableView.dataSource = self
+            tableView.delegate = self
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.title = "GitHub Users"
     }
 
 
 }
-
+extension ViewController: UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: cellId)
+        cell.textLabel?.text = "cell"
+        return cell
+    }
+    
+    
+}
